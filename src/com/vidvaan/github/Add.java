135dc1d73ac678b79c2b8658e054b0193db4d89e
navee <navee@DESKTOP-LBLL6C0>
2019-08-08 15:12:51 +0530

    GuuavHVASDVJAS....

---
+++ src/com/vidvaan/github/Add.java
@@ -0,0 +1,12 @@
+package com.vidvaan.github;
+
+public class Add {
+	int m1(int a) {
+		return a;
+	}
+	public static void main(String[] args) {
+		Add a = new Add();
+		int b = a.m1(20);
+		System.out.println(b);
+	}
+}
