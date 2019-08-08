d7984632216a8a7dae558e2d3f308aa477d96d0e
navee <navee@DESKTOP-LBLL6C0>
2019-08-08 15:48:11 +0530

    I Added Emp class

---
+++ src/com/vidvaan/github/Emp.java
@@ -0,0 +1,12 @@
+package com.vidvaan.github;
+
+public class Emp {
+	String emp(String name) {
+		return name;
+	}
+	public static void main(String[] args) {
+		Emp e = new Emp();
+		System.out.println(e.emp("nani"));
+	}
+	
+}
