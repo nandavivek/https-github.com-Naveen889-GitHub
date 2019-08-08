fd3c19af2cfef0230e0203866e94ce012a2feead
navee <navee@DESKTOP-LBLL6C0>
2019-08-08 15:27:08 +0530

    I Added Car java Class

---
+++ src/com/vidvaan/github/Car.java
@@ -0,0 +1,13 @@
+package com.vidvaan.github;
+
+public class Car {
+	String m2(String name) {
+		return name;
+	}
+
+	public static void main(String[] args) {
+		Car c = new Car();
+		String b = c.m2("Bench");
+		System.out.println(b);
+	}
+}
